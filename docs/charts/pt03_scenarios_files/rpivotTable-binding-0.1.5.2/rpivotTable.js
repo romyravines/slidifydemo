27316f30551ef2b090f4039eb2e3aa3215f1aaaf
@romyravines <romy.elena@gmail.com>
2020-07-24 19:08:25 +0200

    demo01

---
+++ docs/charts/pt03_scenarios_files/rpivotTable-binding-0.1.5.2/rpivotTable.js
@@ -0,0 +1,36 @@
+HTMLWidgets.widget({
+
+    name: 'rpivotTable',
+
+    type: 'output',
+
+    initialize: function(el, width, height) {
+
+	    return {}
+
+    },
+
+    renderValue: function(el, x, instance) {
+	    x.data = HTMLWidgets.dataframeToD3(x.data);
+
+	    var derivers = $.pivotUtilities.derivers;
+      var tpl = $.pivotUtilities.aggregatorTemplates;
+
+      x.params.renderers = $.extend(
+        $.pivotUtilities.renderers,
+        $.pivotUtilities.d3_renderers,
+        $.pivotUtilities.c3_renderers
+      );
+
+
+      $('#'+el.id).pivotUI(
+      		x.data, x.params
+      );
+
+    },
+
+    resize: function(el, width, height, instance) {
+
+    }
+
+});
